ea7a52539e9ed67420a94c503921e99a06f30c82
zyj <1305696765@qq.com>
2021-08-30 18:21:39 +0800

    kenerl 模块提供系统调用

--- test/module/000-hello/hello.c
+++ test/module/000-hello/hello.c
@@ -5,17 +5,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-  static int __init hello_init(void)
+static int __init 
+hello_init(void)
 {
-	    printk(KERN_INFO "Hello World enter.\n");
-		    return 0;
+	printk(KERN_INFO "Hello World enter.\n");
+	return 0;
 }
 
 module_init(hello_init);
 
-static void __exit hello_exit(void)
+static void __exit 
+hello_exit(void)
 {
-	    printk(KERN_INFO "Hello world exit.\n");
+	printk(KERN_INFO "Hello world exit.\n");
 }
 
 module_exit(hello_exit);
